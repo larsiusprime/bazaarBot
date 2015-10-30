a20ad8a828b545aec74b9101a773f6005d4a16fb
Paul Cochrane <paul@liekut.de>
2015-10-30 10:33:36 +0100

    Purge trailing whitespace in bazaarbot/agent dir

--- bazaarbot/agent/LogicHScript.hx
+++ bazaarbot/agent/LogicHScript.hx
@@ -13,8 +13,8 @@ class LogicHScript extends Logic
 {
 	var script:String = "";
 	var source:String;
-	
-	public function new(?data:Dynamic) 
+
+	public function new(?data:Dynamic)
 	{
 		super(data);
 		if (data == null) return;
@@ -23,33 +23,33 @@ class LogicHScript extends Logic
 			script = Assets.getText("assets/scripts/"+data);
 		}
 	}
-	
-	override public function perform(agent:Agent, bazaar:Market):Void 
+
+	override public function perform(agent:Agent, bazaar:Market):Void
 	{
 		_perform_script(script, agent, bazaar);
 	}
-	
+
 	private function _perform_script(script:String, agent:Agent, bazaar:Market):Void
 	{
 		var parser = new Parser();
 		var ast = parser.parseString(script);
 		var interp = new Interp();
-		
-		var vars:Map<String,Dynamic> = 
+
+		var vars:Map<String,Dynamic> =
 		[
-		 "agent" => agent, 
+		 "agent" => agent,
 		 "query_inventory" => agent.queryInventory,
 		 "produce" => _produce,
 		 "consume" => _consume,
 		 "inventory_is_full" => agent.get_inventoryFull,
-		 "make_room_for" => 
+		 "make_room_for" =>
 			function(a:Agent, c:String = "food", amt:Float = 1.0):Void
-			{ 
+			{
 				var to_drop:String = bazaar.getCheapestCommodity(10, [c]);
 				if (to_drop != "") {_consume(a, to_drop, amt);}
 			}
 		 ];
-		 
+
 		interp.variables = vars;
 		interp.execute(ast);
 	}
